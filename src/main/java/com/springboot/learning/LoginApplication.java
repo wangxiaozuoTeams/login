a83d2e69be8262e60af87532858ee091c729f823
mailw <wangxiaozuo@hayll.cn>
2019-01-10 12:00:44 +0800

    ok

---
+++ src/main/java/com/springboot/learning/LoginApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: wangjian
+ * @Date: 2019/1/10 11:58
+ * @Description:
+ */
+@SpringBootApplication
+public class LoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+}
